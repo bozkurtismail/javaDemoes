8c24a6b057dc492d2a816d9c6ff1f443cf123e79
ismail BOZKURT <ismailbozkurt@ymail.com>
2022-10-27 00:13:55 +0300

    classes learn

---
+++ 16-classes/src/CustomerManager.java
@@ -0,0 +1,16 @@
+public class CustomerManager {
+
+    public void add(){
+        System.out.println("Customer Added");
+    }
+
+    public void remove(){
+        System.out.println("Customer Deleted");
+    }
+
+    public void update(){
+        System.out.println("Customer Updated");
+    }
+
+
+}
